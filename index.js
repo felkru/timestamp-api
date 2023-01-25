4daf2219803a7a15f84172d21f70b4fcaad75f7c
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-01-25 09:47:19 +0000

    finished, functional

--- index.js
+++ index.js
@@ -2,31 +2,46 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/", function (req, res) {
+  let date = new Date();
+  let unix = date.getTime();
+  date = date.toUTCString();
+  res.json({ unix: unix, utc: date });
 });
 
-
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  // format date as Fri, 25 Dec 2015 00:00:00 GMT
+  let date = /\D/.test(req.params.date)
+    ? new Date(req.params.date)
+    : new Date(parseInt(req.params.date));
+  // convert date to unix
+  let unix = date.getTime();
+  date = date.toUTCString();
+  if (date === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+  res.json({ unix: unix, utc: date });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
